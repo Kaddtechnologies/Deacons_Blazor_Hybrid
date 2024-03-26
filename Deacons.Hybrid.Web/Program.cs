8db3bdf3b8a936ac9515b246fe9c25afc18584ea
David Jones <davidml3@yahoo.com>
2024-03-26 16:06:12 -0500

    Add Edit Details

--- Deacons.Hybrid.Web/Program.cs
+++ Deacons.Hybrid.Web/Program.cs
@@ -1,4 +1,5 @@
 using Deacons.Hybrid.Web.Components;
+using MudBlazor.Services;
 
 namespace Deacons.Hybrid.Web
 {
@@ -11,6 +12,7 @@ namespace Deacons.Hybrid.Web
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
+            builder.Services.AddMudServices();
 
             var app = builder.Build();
 
